f6d4a2ec888b7bd6721906611b25c25c12034562
Igor marques <igormarquespe@gmail.com>
2021-10-20 19:54:48 -0300

    Entity Order, Instant

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,20 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
 	
@@ -17,12 +23,16 @@ public class User implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User (){
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public User(){
 		
 	}
 
@@ -75,6 +85,11 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +106,8 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return id == other.id;
 	}
+
+	
 
 
 
