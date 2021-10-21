d087959d90d805c176045d8f749939a1477a9050
Igor marques <igormarquespe@gmail.com>
2021-10-21 19:45:32 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -11,8 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -22,9 +22,6 @@ import lombok.Setter;
 
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_order")
@@ -41,8 +38,58 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "MGT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public  OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus) ;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+			if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+			}
+		}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+	
 }
